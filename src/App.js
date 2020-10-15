0973f6e4386e938fe66322fe438dcfc24a963e81
Earvin <earvin.tli@gmail.com>
2020-10-15 17:01:39 +0800

    [App] feat: build a basic fetch 'GraphQL' app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fetchGraphQL from './fetchGraphQL';
+
+const {
+  useEffect,
+  useState,
+} = React;
 
 function App() {
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        repository(owner: "facebook" name: "relay") {
+          name
+        }
+      }
+    `).then(response => {
+      if (!isMounted) return;
+      console.log(response);
+      setName(response.data.repository.name);
+    }).catch(error => console.error(error));
+  }, [fetchGraphQL]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{name !== null ? `Repository: ${name}` : "Loading"}</p>
       </header>
     </div>
   );
